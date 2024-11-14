326edee04b2872d3253e8d95bd5014abde05c407
LeoBasso <127260629+LeoBasso@users.noreply.github.com>
2024-11-13 23:02:40 -0300

    alterar status corrigido

--- server/src/modules/schedules/services/UpdateScheduleService.ts
+++ server/src/modules/schedules/services/UpdateScheduleService.ts
@@ -17,8 +17,8 @@ export class UpdateScheduleService {
 
   public async execute(updateSchedule: UpdateScheduleDTO): Promise<ScheduleResponseDTO> {
     const scheduleDate = new Date(updateSchedule.date);
-
     const dayOfWeek = scheduleDate.getDay();
+
     if (dayOfWeek === 0 || dayOfWeek === 6) {
       throw new BadRequestError('Não é possível criar agendamentos aos sábados e domingos');
     }
@@ -27,60 +27,60 @@ export class UpdateScheduleService {
       throw new BadRequestError('Não é possível criar agendamentos em datas anteriores a hoje');
     }
 
-    const [hour, minutes] = updateSchedule.hour.split(':').map(Number);
-    scheduleDate.setHours(hour, minutes, 0, 0);
-    
     const schedule = await this.schedulesRepository.findById(updateSchedule.id);
-
-    const service = await this.servicesRepository.findById(updateSchedule.service_id);
-
-    if(!schedule){
-      throw new BadRequestError('Schedule not founded')
+    if (!schedule) {
+      throw new BadRequestError('Schedule not found');
     }
 
-    const endhourDate = new Date(scheduleDate.getTime() + service.time * 60 * 1000);
-    const formattedEndhour = `${endhourDate.getHours().toString().padStart(2, '0')}:${endhourDate.getMinutes().toString().padStart(2, '0')}`;
+    if (updateSchedule.hour) {
+      const [hour, minutes] = updateSchedule.hour.split(':').map(Number);
+      scheduleDate.setHours(hour, minutes, 0, 0);
+      schedule.hour = updateSchedule.hour as ScheduleHours;
 
-    const exists = await this.schedulesRepository.findByDate({
-      date: updateSchedule.date,
-      startHour: updateSchedule.hour,
-      endHour: formattedEndhour,
-    });
+      const service = await this.servicesRepository.findById(updateSchedule.service_id);
 
-    if (exists) {
-      throw new BadRequestError('Já existe outro agendamento neste horário');
+      const endhourDate = new Date(scheduleDate.getTime() + service.time * 60 * 1000);
+      const formattedEndhour = `${endhourDate.getHours().toString().padStart(2, '0')}:${endhourDate.getMinutes().toString().padStart(2, '0')}`;
+      schedule.endhour = formattedEndhour;
+
+      const exists = await this.schedulesRepository.findByDate({
+        date: updateSchedule.date,
+        startHour: updateSchedule.hour,
+        endHour: formattedEndhour,
+      });
+
+      if (exists) {
+        throw new BadRequestError('Já existe outro agendamento neste horário');
+      }
+
+      const allSchedules = await this.schedulesRepository.findAll();
+      const conflict = allSchedules.some((existingSchedule) => {
+        const scheduleStart = new Date(`${updateSchedule.date}T${updateSchedule.hour}:00`);
+        const scheduleEnd = new Date(scheduleStart.getTime() + service.time * 60 * 1000);
+
+        const existingScheduleStart = new Date(`${existingSchedule.date}T${existingSchedule.hour}:00`);
+        const existingScheduleEnd = new Date(existingScheduleStart.getTime() + service.time * 60 * 1000);
+
+        return (scheduleStart < existingScheduleEnd && scheduleEnd > existingScheduleStart);
+      });
+
+      if (conflict) {
+        throw new BadRequestError('O tempo do serviço não se ajusta ao horário selecionado');
+      }
     }
 
-    const allSchedules = await this.schedulesRepository.findAll();
-
-    const conflict = allSchedules.some((schedule) => {
-      const scheduleStart = new Date(`${updateSchedule.date}T${updateSchedule.hour}:00`);
-      const scheduleEnd = new Date(scheduleStart.getTime() + service.time * 60 * 1000);
-
-      const existingScheduleStart = new Date(`${schedule.date}T${schedule.hour}:00`);
-      const existingScheduleEnd = new Date(existingScheduleStart.getTime() + service.time * 60 * 1000);
-
-      return (scheduleStart < existingScheduleEnd && scheduleEnd > existingScheduleStart);
-    });
-
-    if (conflict) {
-      throw new BadRequestError('O tempo do serviço não se ajusta ao horário selecionado');
+    if (updateSchedule.status) {
+      schedule.status = updateSchedule.status;
     }
-
-
-    schedule.date = updateSchedule.date;
-    schedule.hour = updateSchedule.hour as ScheduleHours;
-    schedule.endhour = updateSchedule.hour;
-    schedule.status = updateSchedule.status;
 
     const response = await this.schedulesRepository.save(schedule);
 
     return new ScheduleResponseDTO(
-      response.id, 
-      response.date, 
+      response.id,
+      response.date,
       response.hour,
       response.endhour,
-      response.status, 
+      response.status,
     );
   }
 }
